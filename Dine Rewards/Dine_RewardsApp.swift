b0d4983d46aed9ecc4125baefd89c90b98be20a0
ramachandran-rahul <ramachandran.k.rahul@gmail.com>
2024-05-07 17:24:01 +1000

    Initial Commit

---
+++ Dine Rewards/Dine_RewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dine_RewardsApp.swift
+//  Dine Rewards
+//
+//  Created by Rahul Ramachandran on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Dine_RewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
