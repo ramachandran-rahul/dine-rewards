e36ddf98a8ef59adb92759a166df6c28432b9615
ramachandran-rahul <ramachandran.k.rahul@gmail.com>
2024-05-17 18:54:42 +1000

    added svprogresshud to contentview for initial splash screen

--- Dine Rewards/Views/ContentView.swift
+++ Dine Rewards/Views/ContentView.swift
@@ -6,21 +6,50 @@
 //
 
 import SwiftUI
+import SVProgressHUD
 
 struct ContentView: View {
     @StateObject var auth: AuthManager = AuthManager.shared
+    @State private var isLoading = true
 
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.black.edgesIgnoringSafeArea(.all)
-                if !auth.isLoggedIn {
-                    LoginView() // Redirects to the Login View after delay
-                } else {
-                    if(auth.user != nil && auth.user?.phoneNumber != nil) {
-                        ListRestaurantView(phoneNumber: auth.user!.phoneNumber!)
+                
+                if isLoading {
+                     VStack {
+                         Spacer()
+                         Image("dineRewardsLogo")
+                             .resizable()
+                             .aspectRatio(contentMode: .fit)
+                             .frame(width: 200, height: 200)
+                             .padding(.bottom, 125)
+                         Spacer()
+                     }
+                     .onAppear {
+                         // Customise SVProgressHUD appearance
+                         SVProgressHUD.setDefaultStyle(.custom)
+                         SVProgressHUD.setBackgroundColor(.clear)
+                         SVProgressHUD.setForegroundColor(.white)
+                         SVProgressHUD.setRingThickness(7.0)
+                         SVProgressHUD.setOffsetFromCenter(UIOffset(horizontal: 0, vertical: 125))
+                         SVProgressHUD.show()
+
+                         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                             SVProgressHUD.dismiss()
+                             isLoading = false
+                         }
+                     }
+                } else { // Redirects to the relevant view after delay
+                    if !auth.isLoggedIn {
+                        LoginView()
                     } else {
-                        EmptyView()
+                        if(auth.user != nil && auth.user?.phoneNumber != nil) {
+                            ListRestaurantView(phoneNumber: auth.user!.phoneNumber!)
+                        } else {
+                            EmptyView()
+                        }
                     }
                 }
             }
